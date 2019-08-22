ebcb3aedca97b771957baf24aeed68a2ff558bf0
zy <1165045817@qq.com>
2019-08-23 00:52:28 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,63 @@
 <template>
     <div class="container">
-    <el-card class="my-card">
+        <el-card class="box-card">
+            <img src="../../assets/images/logo_index.png" style="margin-bottom: 40px">
 
-    </el-card>
+            <el-form :model="loginForm" style="">
+                <el-form-item>
+                <el-input v-model="loginForm.mobile" placeholder="请输入你的手机号"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                <el-input v-model="loginForm.code" placeholder="请输入你的验证码" style="width: 265px"></el-input>
+                <el-button style="margin-left: 10px">验证码</el-button>
+                </el-form-item>
+
+                <el-button type="primary" round style="width: 100%">登录</el-button>
+            </el-form>
+
+        </el-card>
+
     </div>
 
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+
 }
 </script>
 
 <style lang="less" scoped>
+.container {
+    background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0
+}
+
+  .box-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    top: 20%;
+    left: 30%;
+
+    img {
+        width: 200px;
+        display: block;
+        margin: 0 auto;
+    }
+  }
+
 </style>
